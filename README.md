c23c9e47fe07b22278b47a358bf5dfc5f2463ea9
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2022-12-18 20:50:58 +0100

    Some ideas

--- README.md
+++ README.md
@@ -6,6 +6,27 @@ SPDX-License-Identifier: AGPL-3.0-or-later
 
 # uring-crdt
 
+## Documentation (also of dependencies because git dependencies)
+
+```bash
+cargo doc --open
+```
+
+## crdt design
+
+We need to use an Operation-based CRDT and not a State-based CRDT because we want to know who changed what and also add a permission system.
+
+We need to authenticate each operation by the author of the operation using cryptography.
+
+We should probably cache the current value of the CRDTs underlying value for efficiency.
+
+But for now we first need to persist the CRDT itself.
+
+As CRDT nodes need to reference (multiple) previous nodes (similar to git) this is a Directed acyclic graph.
+
+It could be a kind of append only log as the dependency order creates a natural order of the elements. There is no unique order though.
+
+Then there would be one file per element. Maybe later we could create an abstraction that does this in the database itself so we rely less on the file system.
 
 ## Update license headers
 
--- src/main.rs
+++ src/main.rs
@@ -2,23 +2,54 @@
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
-use tokio_uring::fs::File;
+use std::error::Error;
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tokio_uring::start(async {
-        // Open a file
-        let file = File::open("hello.txt").await?;
+use rustls::Certificate;
+use tokio::{fs::{OpenOptions, File}, io::{AsyncWriteExt, AsyncReadExt}};
 
-        let buf = vec![0; 4096];
-        // Read some data, the buffer is passed by ownership and
-        // submitted to the kernel. When the operation completes,
-        // we get the buffer back.
-        let (res, buf) = file.read_at(buf, 0).await;
-        let n = res?;
+pub struct MyIdentity {
+    // public and private key
+    certificate: Certificate
+}
 
-        // Display the contents
-        println!("{:?}", &buf[..n]);
+impl MyIdentity {
+    pub async fn new() -> Result<Self, Box<dyn Error>> {
+        let cert = rcgen::generate_simple_self_signed(vec![])?;
+        // don't accidentially overwrite the file
+        let mut file = OpenOptions::new().create_new(true).open("private-key.pem").await?;
+        file.write_all(&cert.serialize_private_key_der()).await?;
+        file.sync_all().await?; 
 
-        Ok(())
-    })
+        let mut file = OpenOptions::new().read(true).open("private-key.pem").await?;
+        let mut buffer = Vec::new();
+        file.read_to_end(&mut buffer).await?;
+
+        Ok(MyIdentity { 
+            certificate: Certificate(buffer)
+         })
+    }
+}
+
+pub struct CmRDT {
+    file: File,
+}
+
+impl CmRDT {
+
+    pub async fn new() -> Result<Self, Box<dyn Error>> {
+
+        Ok(Self {
+            file: File::open("hello.txt").await?
+        })
+    }
+}
+
+fn main() {
+    tokio::runtime::Builder::new_multi_thread()
+        .enable_all()
+        .build()
+        .unwrap()
+        .block_on(async {
+            println!("Hello world");
+        })
 }
